0953af0a72538d68f78e1a72d93095427f15b555
Kavya <kavyah799@gmail.com>
2025-12-16 21:42:03 +0530

    Fix capitalization in employee details test
    Corrected the capitalization of 'Employee Name' to 'Employee name' in the expected output.

--- test_employee.py
+++ test_employee.py
@@ -1,7 +1,7 @@
 from employee import employee_details
 def test_employee_details():
     expected_output = (
-        "Employee Name: Alex\n"
+        "Employee name: Alex\n"
         "Employee ID:001\n"
         "Department: IT\n"
         "Salary: 55000\n"
