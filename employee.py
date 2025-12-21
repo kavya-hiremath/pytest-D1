311cddff45bfdf14bda24fbbb76b4a04d083ee85
Kavya <kavyah799@gmail.com>
2025-12-21 20:39:29 +0530

    Refactor employee_details function and main block

--- employee.py
+++ employee.py
@@ -1,15 +1,17 @@
-def employee_details(name,emp_id,department,salary) :
-    result=(
-        f"Employee name:{name}\n"
-        f"Employee ID:{emp_id}\n"
-        f"Department:{department}\n"
-        f"Salary:{salary}\n"
+def employee_details(name, emp_id, department, salary):
+    result = (
+        f"Employee Name: {name}\n"
+        f"Employee ID: {emp_id}\n"
+        f"Department: {department}\n"
+        f"Salary: {salary}"
     )
     return result
-  
-if __name__=="__main__":
-    name="Alex"
-    emp_id="001"
-    department="IT"
-    salary=55000
+
+
+if __name__ == "__main__":
+    name = "keerthi"
+    emp_id = "E123"
+    department = "Tech"
+    salary = 80000
+
     print(employee_details(name, emp_id, department, salary))
